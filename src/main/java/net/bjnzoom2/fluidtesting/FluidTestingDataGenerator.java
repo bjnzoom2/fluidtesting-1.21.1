bb6fdbafbea4dc5c5ed00271221a2739e3f9a783
bjnzoom2 <bjnzoom2@gmail.com>
2025-08-15 20:56:10 +0800

    Initial commit

---
+++ src/main/java/net/bjnzoom2/fluidtesting/FluidTestingDataGenerator.java
@@ -0,0 +1,11 @@
+package net.bjnzoom2.fluidtesting;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class FluidTestingDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
