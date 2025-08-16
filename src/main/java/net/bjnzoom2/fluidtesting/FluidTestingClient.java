c3e24acab630bd4e62ad9c426c7dfa0e2bc46123
bjnzoom2 <bjnzoom2@gmail.com>
2025-08-16 13:29:53 +0800

    Commit

---
+++ src/main/java/net/bjnzoom2/fluidtesting/FluidTestingClient.java
@@ -0,0 +1,10 @@
+package net.bjnzoom2.fluidtesting;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class FluidTestingClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
